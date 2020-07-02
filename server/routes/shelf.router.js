e85eb291e6fbbfec0728ccb10c47e581aba2e619
Jake Schaffer <jakecr8guru@gmail.com>
2020-07-02 16:34:43 -0500

    we did things like test routes and displayed content and had a good time

--- server/routes/shelf.router.js
+++ server/routes/shelf.router.js
@@ -1,28 +1,78 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
+const { rejectUnauthenticated } = require('../modules/authentication-middleware');
 
 /**
  * Get all of the items on the shelf
  */
 router.get('/', (req, res) => {
-    res.sendStatus(200); // For testing only, can be removed
+    let queryText = `SELECT * FROM item`;
+    pool.query(queryText)
+        .then((result) => {
+            res.status(200).send(result.rows);
+        }).catch((error) => {
+            console.log(error);
+            res.sendStatus(500);
+        });
 });
 
 
 /**
- * Add an item for the logged in user to the shelf
+ * Add an item for the logged-in user to the shelf
  */
-router.post('/', (req, res) => {
-
+router.post('/', rejectUnauthenticated, (req, res) => {
+  if (req.isAuthenticated() === false) {
+    res.sendStatus(403);
+    return;
+  }
+  const item = req.body; 
+  const user = req.user; // user authenticated
+  console.log(user);
+  console.log(item);
+  
+  const queryText = `INSERT INTO item (description, image_url, user_id) VALUES ($1, $2, $3)`;
+  const queryThing = [item.description, item.image_url, user.id];
+  console.log(queryThing);
+  
+  pool
+    .query(queryText, queryThing)
+    .then(() => {
+      console.log('query worked to DB');
+      res.sendStatus(201)
+    })
+    .catch((error) => {
+      console.log(error);
+      res.sendStatus(500);
+    });
 });
 
 
-/**
- * Delete an item if it's something the logged in user added
- */
-router.delete('/:id', (req, res) => {
 
+/* Delete an item if it's something the logged in user added */
+router.delete('/:id', rejectUnauthenticated, (req, res) => {
+    const queryText = `SELECT * FROM item WHERE id=$1`;
+    pool.query(queryText, [req.params.id])
+        .then(result => {
+            if (result.rows.length === 0) {
+                res.sendStatus(401);
+                return;
+            }
+            const item = result.rows[0];
+            if (item.user_id !== req.user.id) {
+                res.sendStatus(418).send('You cant remove someone elses item.');
+                return;
+            } else {
+                // its their item! allow them delete it
+                const queryText2 = `DELETE FROM item WHERE id=$1`;
+                pool.query(queryText2, [item.id])
+                    .then(() => res.sendStatus(200))
+                    .catch((error) => {
+                        console.log(error);
+                        res.sendStatus(500);
+                     });
+            }     
+        });
 });
 
 
--- src/components/InfoPage/InfoPage.js
+++ src/components/InfoPage/InfoPage.js
@@ -1,16 +1,40 @@
-import React from 'react';
+import React, { useState } from "react";
+import axios from 'axios';
 
 // This is one of our simplest components
 // It doesn't have local state, so it can be a function component.
 // It doesn't dispatch any redux actions or display any part of redux state
 // or even care what the redux state is, so it doesn't need 'connect()'
 
-const InfoPage = () => (
-  <div>
-    <p>
-      Shelf Page
-    </p>
-  </div>
-);
-
+const InfoPage = () => {
+  const [resData, setResData] = useState(null);
+  const getFun = () => {
+    axios
+      .get("/api/shelf")
+      .then((response) => {
+        setResData(response.data);
+      }
+      );
+  }
+  return (
+    <div>
+      <p>Shelf Page</p>
+      <button onClick={getFun}>Click</button>
+      <div>
+        {resData !== null ? (
+          <div>
+            {resData.map((item, index) => (
+              <div key={index}>
+                <h1>{item.description}</h1>
+                <img src={item.image_url} height='300' width='350'/>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <p>?</p>
+        )}
+      </div>
+    </div> 
+  );
+}
 export default InfoPage;
